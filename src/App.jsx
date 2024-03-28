d8cfde758c50baf45f6376efccf1f5e37199575a
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-03-28 01:49:00 +0100

    back to the top

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./Styles.scss";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import BackToTopButton from "./Components/BackToTopButton";
 
 const App = () => {
   return (
 <main>
 <Navbar />
 <Home />
+<BackToTopButton/>
 </main>
   );
 };
